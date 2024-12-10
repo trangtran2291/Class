143ad92b78ae84f5b658d1161ac2adc8d8b2afe8
trangtran2291 <trangtran.31231022291@st.ueh.edu.vn>
2024-12-10 16:38:54 +0700

    ChessLogic_Video01_Video07

---
+++ MoveType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public enum MoveType
+    {
+        Normal,
+        CastleKS,
+        CastleQS,
+        DoublePam,
+        EnPassant,
+        PawnPromotion,
+
+
+    }
+}
